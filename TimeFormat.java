c0a47e8d1dc716084fe3a6875fb37f8aa02173a5
samuelelkaim <elkaim.samuel03@gmail.com>
2024-12-01 00:01:46 +0200

    Update TimeFormat.java

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,17 +1,15 @@
-// Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
-	public static void main(String[] args) {
-		// In Java, the command-line arguments args[0], args[1], ... are represented
-		// each by a string. In this program, the single "hh:mm" input is represented
-		// by the single command-line string argument args[0]. 
-		//   
-		// The following statement handles the hours part of the input.
-		// It concatenates the empty string "" with the leftmost hour-digit. 
-		// It then concatenates the resulting string with the rightmost hour-digit,
-		// and then uses parseInt to cast the resulting string as an int.
-		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		// Does the same with the minutes part of the input.
-		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
-	}
+    public static void main(String[] args) {
+        String[] timeParts = args[0].split(":");
+        int hours = Integer.parseInt(timeParts[0]);
+        int minutes = Integer.parseInt(timeParts[1]);
+
+        String period = (hours < 12) ? "AM" : "PM";
+        hours = (hours == 0 || hours == 12) ? 12 : hours % 12;
+
+        System.out.printf("%d:%02d %s%n", hours, minutes, period);
+    }
 }
+
+    
+
